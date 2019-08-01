ae8e1ac639c235175b3d49ca710825414ad410c2
unknown <jsmileydev@gmail.com>
2019-08-01 14:27:11 -0500

    Clone single items from a recipe to shopping list, style icons

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ $(document).ready(function() {
       $('#recipe-body').append(
         '<ul class="recipe-list" id="' +
           newRecipe +
-          '-list"><h3 class="recipe-name"><i class="fas fa-folder-plus add-recipe-icon"></i> ' +
+          '-list"><h3 class="recipe-name"> ' +
           newRecipe +
-          '</h3><input type="text" id="' +
+          '<i class="fas fa-folder-plus add-recipe-icon"></i></h3><input type="text" id="' +
           newRecipe +
           '-item" class="rec-input" placeholder="Add an item"> </ul>'
       );
@@ -52,16 +52,22 @@ $(document).ready(function() {
       var newRecItem = $(this).val();
       $(this).val('').attr('placeholder', 'Add new item');
       console.log(newRecItem);
-      $(this).closest('ul').append('<li class="recipe-item"><i class="fas fa-sort"></i> ' + newRecItem + '</li>');      
+      $(this).closest('ul').append('<li class="recipe-item"><i class="fas fa-sort"></i> ' + newRecItem + '<i class="fas fa-plus-circle add-rec-item-icon"></i></li>');      
     }
     $(this).closest('ul').sortable({items: 'li:not(.recipe-name)'}).disableSelection();
   });
 
-  //On click of icon, add all incredients of recipe to the main shopping list
+  //On click of icon, add all incredients of recipe to the main shopping list, filtering for <li> from <ul> and removing recipe item classese
   $(document).on('click', '.add-recipe-icon', function() {
     var recLi = $(this).closest('ul').contents().filter('li');
-    $(recLi).clone().appendTo('#check-list').removeClass('recipe-item').addClass('list-item');
-  });  
+    $(recLi).clone().appendTo('#check-list').removeClass('recipe-item').addClass('list-item').contents().filter('.add-rec-item-icon').remove();
+  });
+
+  //On click of icon, add single recipe item to the main shopping list, removing recipe item class & add icon
+  $(document).on('click', '.add-rec-item-icon', function() {
+    var recItem = $(this).closest('li');
+    $(recItem).clone().appendTo('#check-list').removeClass('recipe-item').addClass('list-item').contents().filter('.add-rec-item-icon').remove();
+  });
   
 	//Make list sortable
 	$(function() {
