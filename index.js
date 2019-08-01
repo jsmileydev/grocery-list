5749a14d2504e057e9e5b56b78aea7bf77c7453d
unknown <jsmileydev@gmail.com>
2019-08-01 15:14:23 -0500

    Add trash items and style icon with fade

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
 	//Add item from array to <ul> as <li>
 	$('#check-list').each(function(i) {
 		for (var x = 0; x < groceries.length; x++) {
-			$(this).append('<li class="list-item"><i class="fas fa-sort"></i> ' + groceries[x] + '</li>');
+			$(this).append('<li class="list-item"><i class="fas fa-sort"></i> ' + groceries[x] + '<i class="fas fa-trash-alt trash-icon"></i></li>');
 		}
   });
   
-	//On click of list item, cross off and move to bottom of list, or uncross and move back to top
+	//On click of list item, add cross-off style and move to bottom of list, or uncross and move back to top
 	$(document).on('click', '.list-item', function() {
 		if ($(this).hasClass('strike')) {
 			$(this).removeClass('strike');
@@ -20,16 +20,16 @@ $(document).ready(function() {
 		}
   });
   
-  //Add new item to list on keypress
+  //Add new item to main list, taking value from input and prepending to start of main list
   $('#new-item').keypress(function(event) {
     if(event.which === 13) {
       var newItem = $(this).val();
       $(this).val('').attr('placeholder', 'Add new item');
-      $('#check-list').prepend('<li class="list-item"><i class="fas fa-sort"></i> ' + newItem + '</li>');
+      $('#check-list').prepend('<li class="list-item"><i class="fas fa-sort"></i> ' + newItem + '<i class="fas fa-trash-alt trash-icon"></i></li>');
     }
   });
   
-  //Start new recipe list on keypress, with recipe name and individual input
+  //Start new recipe list on keypress, taking recipe name from input and appending as a new <ul> with input value as part of id, as header of new <ul>, and as part of new <input> id
   $('#new-recipe').keypress(function(event) {
     if (event.which === 13) {
       var newRecipe = $(this).val();
@@ -37,39 +37,46 @@ $(document).ready(function() {
       $('#recipe-body').append(
         '<ul class="recipe-list" id="' +
           newRecipe +
-          '-list"><h3 class="recipe-name"> ' +
+          '-list"><i class="fas fa-folder-plus add-recipe-icon"></i><i class="fas fa-trash-alt trash-icon"></i><h3 class="recipe-name"> ' +
           newRecipe +
-          '<i class="fas fa-folder-plus add-recipe-icon"></i></h3><input type="text" id="' +
+          '</h3><input type="text" id="' +
           newRecipe +
           '-item" class="rec-input" placeholder="Add an item"> </ul>'
       );
     }
   });
   
-  //Add new item to individual recipe on keypress
+  //Add new item to individual recipe on keypress, taking value from input and appending as a child <li>
   $(document).on('keypress', '.rec-input', function(event) {
     if (event.which === 13) {
       var newRecItem = $(this).val();
       $(this).val('').attr('placeholder', 'Add new item');
       console.log(newRecItem);
-      $(this).closest('ul').append('<li class="recipe-item"><i class="fas fa-sort"></i> ' + newRecItem + '<i class="fas fa-plus-circle add-rec-item-icon"></i></li>');      
+      $(this).closest('ul').append('<li class="recipe-item"><i class="fas fa-sort"></i> ' + newRecItem + '<i class="fas fa-plus-circle add-rec-item-icon"></i><i class="fas fa-trash-alt trash-icon"></i></li>');
     }
     $(this).closest('ul').sortable({items: 'li:not(.recipe-name)'}).disableSelection();
   });
 
-  //On click of icon, add all incredients of recipe to the main shopping list, filtering for <li> from <ul> and removing recipe item classese
+  //On click of icon, add all incredients of recipe to the main shopping list, filtering for <li> from <ul>, and removing recipe item classes & add icon
   $(document).on('click', '.add-recipe-icon', function() {
     var recLi = $(this).closest('ul').contents().filter('li');
-    $(recLi).clone().appendTo('#check-list').removeClass('recipe-item').addClass('list-item').contents().filter('.add-rec-item-icon').remove();
+    $(recLi).clone().prependTo('#check-list').removeClass('recipe-item').addClass('list-item').contents().filter('.add-rec-item-icon').remove();
   });
 
   //On click of icon, add single recipe item to the main shopping list, removing recipe item class & add icon
   $(document).on('click', '.add-rec-item-icon', function() {
-    var recItem = $(this).closest('li');
-    $(recItem).clone().appendTo('#check-list').removeClass('recipe-item').addClass('list-item').contents().filter('.add-rec-item-icon').remove();
+    var recItem = $(this).parent();
+    $(recItem).clone().prependTo('#check-list').removeClass('recipe-item').addClass('list-item').contents().filter('.add-rec-item-icon').remove();
+  });
+
+  //On click of icon, delete item 
+  $(document).on('click', '.trash-icon', function() {
+    $(this).parent().fadeOut(500, function(){
+      $(this).remove();
+    });	
   });
   
-	//Make list sortable
+	//Make main shopping list sortable
 	$(function() {
     $('#check-list').sortable().disableSelection();
   });
