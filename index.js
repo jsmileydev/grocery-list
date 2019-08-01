564f0e4ac3927568f9f49af5ab5960644cb788ad
unknown <jsmileydev@gmail.com>
2019-08-01 12:57:40 -0500

    Style recipe item add input

--- index.js
+++ index.js
@@ -7,22 +7,8 @@ $(document).ready(function() {
 		for (var x = 0; x < groceries.length; x++) {
 			$(this).append('<li class="list-item"><i class="fas fa-sort"></i> ' + groceries[x] + '</li>');
 		}
-	});
-
-	//Make list sortable
-	$(function() {
-    $('#check-list').sortable().disableSelection();    
   });
   
-  //Add new item to list on keypress
-  $('#new-item').keypress(function(event) {
-    if(event.which === 13) {
-      var newItem = $(this).val();
-      $(this).val('').attr('placeholder', 'Add new item');
-      $('#check-list').prepend('<li class="list-item"><i class="fas fa-sort"></i> ' + newItem + '</li>');
-    }
-  });
-
 	//On click of list item, cross off and move to bottom of list, or uncross and move back to top
 	$(document).on('click', '.list-item', function() {
 		if ($(this).hasClass('strike')) {
@@ -34,19 +20,28 @@ $(document).ready(function() {
 		}
   });
   
+  //Add new item to list on keypress
+  $('#new-item').keypress(function(event) {
+    if(event.which === 13) {
+      var newItem = $(this).val();
+      $(this).val('').attr('placeholder', 'Add new item');
+      $('#check-list').prepend('<li class="list-item"><i class="fas fa-sort"></i> ' + newItem + '</li>');
+    }
+  });
+  
   //Start new recipe list on keypress, with recipe name and individual input
   $('#new-recipe').keypress(function(event) {
     if (event.which === 13) {
       var newRecipe = $(this).val();
       $(this).val('').attr('placeholder', 'Add new recipe');
-      $('#recipe-body').prepend(
+      $('#recipe-body').append(
         '<ul class="recipe-list" id="' +
           newRecipe +
           '-list"><h3 class="recipe-name"><i class="fas fa-folder-plus add-recipe-icon"></i> ' +
           newRecipe +
-          '<span><input type="text" id="' +
+          '</h3><input type="text" id="' +
           newRecipe +
-          '-item" class="rec-input" placeholder="Add an item"></span></h3></ul>'
+          '-item" class="rec-input" placeholder="Add an item"> </ul>'
       );
     }
   });
@@ -62,10 +57,16 @@ $(document).ready(function() {
     $(this).closest('ul').sortable({items: 'li:not(.recipe-name)'}).disableSelection();
   });
 
+  //On click of icon, add all incredients of recipe to the main shopping list
   $(document).on('click', '.add-recipe-icon', function() {
     var recLi = $(this).closest('ul').contents().filter('li');
-    console.log(recLi);
-    $(recLi).clone().appendTo('#check-list');
-  })  
+    $(recLi).clone().appendTo('#check-list').removeClass('recipe-item').addClass('list-item');
+  });  
+  
+	//Make list sortable
+	$(function() {
+    $('#check-list').sortable().disableSelection();
+  });
+
 
 });
